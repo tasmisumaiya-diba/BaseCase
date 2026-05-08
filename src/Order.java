8cafc1f38470657070791f025d5798938aa50490
tasmisumaiya-diba <tasmisumaiya14@gmail.com>
2026-05-08 13:50:22 +0600

    exception in constructor

--- src/Order.java
+++ src/Order.java
@@ -3,24 +3,25 @@ public class Order
         public String name;
         private double price;
         private int quantity;
-        Order(String name, double price, int quantity)
-        {
+        Order(String name, double price, int quantity)throws Exception {
             this.name = name;
             this.price = price;
             this.quantity = quantity;
-        }
-    public void setPrice(double price)throws Exception
-    {
-        if (price <= 0)
-            throw new Exception("price can't be 0 or negative");
-        this.price = price;
-    }
-        public void setQuantity(int quantity) throws Exception
-        {
+            if (price <= 0)
+                throw new Exception("price can't be 0 or negative");
+            this.price = price;
             if (quantity <= 0)
                 throw new Exception("quantity can't be 0 or negative");
             this.quantity = quantity;
         }
+            public void setPrice ( double price)
+            {
+                this.price = price;
+            }
+            public void setQuantity ( int quantity) throws Exception
+            {
+                this.quantity = quantity;
+            }
         public double total()
         {
            double Total = (price*quantity);
